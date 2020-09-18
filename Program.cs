b373fbdcc337e522e8eb0e046f6f69f23b3f12d6
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-09-18 14:11:12 -0300

    Este commit altera a tela inicial do repositório

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite algo: ");
+            string texto = Console.ReadLine();
+            Console.Write(texto.ToUpper());
+            Console.WriteLine("!!!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Gritador
 Exercício "Gritador" resposta
+
+
+**Enunciado**
+
+aça um programa repita exatamente que o usuário digitar, gritando (em CAIXA ALTA).
+
+**Como executar**
+
+Abra o projeto e digite:
+
+dotnet run
